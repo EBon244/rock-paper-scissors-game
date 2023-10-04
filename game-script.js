e1d94c76bfe7e8366daecb714efe795b5825399b
EBon244 <132640232+EBon244@users.noreply.github.com>
2023-10-04 03:56:08 -0500

    Added function to play a single round game

--- game-script.js
+++ game-script.js
@@ -1,12 +1,35 @@
 console.log("Hello World!")
 
-//const choices = ["Rock", "Paper", "Scissors"];
-
-function getComputerChoice() {
+function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"];
     const random = choices[Math.floor(Math.random() * choices.length)];
     return random;
 }
 
+function playRound(playerSelection, computerSelection){
+    
+    if (playerSelection == computerSelection){
+        return "Tie Game!"
 
-console.log(getComputerChoice())
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") 
+    || (playerSelection == "Paper" && computerSelection == "Rock") 
+    || (playerSelection == "Scissors" && computerSelection == "Paper")){
+        return "You Win! " + playerSelection + " beats " + computerSelection + ".";
+
+    } else {
+        return "You Lose! " + computerSelection + " beats " + playerSelection + ".";
+
+    } 
+
+}
+
+// When you work on this next make "or" statements in the if sections of playRound to make it concise as possible
+// Also move Tie game scenario up top for faster results
+
+
+let playerChoice = prompt();
+const playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
