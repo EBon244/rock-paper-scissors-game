0addd548e29f403646119adf9dbab7af32c09beb
EBon244 <132640232+EBon244@users.noreply.github.com>
2023-10-04 04:59:20 -0500

    Game is mostly complete just needs touch up

--- game-script.js
+++ game-script.js
@@ -1,35 +1,54 @@
 console.log("Hello World!")
 
-function getComputerChoice(){
+let userScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const random = choices[Math.floor(Math.random() * choices.length)];
     return random;
 }
 
-function playRound(playerSelection, computerSelection){
-    
-    if (playerSelection == computerSelection){
+function playRound() {
+
+    let playerChoice = prompt();
+    const playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+    const computerSelection = getComputerChoice();
+
+    if (playerSelection == computerSelection) {
         return "Tie Game!"
 
-    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") 
-    || (playerSelection == "Paper" && computerSelection == "Rock") 
-    || (playerSelection == "Scissors" && computerSelection == "Paper")){
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors")
+        || (playerSelection == "Paper" && computerSelection == "Rock")
+        || (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        userScore++;
         return "You Win! " + playerSelection + " beats " + computerSelection + ".";
 
     } else {
+        computerScore++;
         return "You Lose! " + computerSelection + " beats " + playerSelection + ".";
 
-    } 
+    }
 
 }
 
-// When you work on this next make "or" statements in the if sections of playRound to make it concise as possible
-// Also move Tie game scenario up top for faster results
 
 
-let playerChoice = prompt();
-const playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-const computerSelection = getComputerChoice();
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection,computerSelection));
+function game () {
+
+    console.log(playRound());
+    if (userScore < 5 && computerScore < 5){
+        game();
+    } else {
+        endGame();
+    }
+}
+
+
+function endGame(){
+    if (userScore > computerScore) {
+        console.log("You Win! :)");
+    } else {
+        console.log("You Lose! :(")
+    }
+}
