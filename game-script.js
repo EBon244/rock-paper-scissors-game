21fef0f30b8416b9a51e03109f32af241f472f16
EBon244 <132640232+EBon244@users.noreply.github.com>
2024-06-01 18:02:46 -0500

    Using The Dom to update an old project

--- game-script.js
+++ game-script.js
@@ -1,7 +1,37 @@
+/* TODO Come back at a later point when more knowledge is acquired and re-work this code
+*/
+
+
+
 console.log("Hello World!")
 
 let userScore = 0;
 let computerScore = 0;
+let startGame = document.getElementById("start");
+let scoreDisplay = document.getElementById("scoreDisplay"); // displays winner of recent round played
+let currentScore = document.getElementById("currentScore"); // displays running scoring so far
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => { // button function to choose option for each round
+    button.addEventListener("click", () => {
+
+        if (button.id.includes("rock")) {
+            console.log(scoreDisplay.textContent = playRound("Rock"));
+            game();
+        } else if (button.id.includes("paper")) {
+            console.log(scoreDisplay.textContent = playRound("Paper"));
+            game();
+        } else if (button.id.includes("scissors")) {
+            console.log(scoreDisplay.textContent = playRound("Scissors"));
+            game();
+        } else { 
+            userScore = 0;
+            computerScore = 0;
+            displayScore();
+        }
+
+    });
+});
 
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
@@ -9,19 +39,23 @@ function getComputerChoice() {
     return random;
 }
 
-function playRound() {
+// function getPlayerChoice(){
+//     let playerChoice = prompt();
+//     return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+// }
 
-    let playerChoice = prompt();
-    const playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+function playRound(choice) {
+
+    const playerSelection = choice;
     const computerSelection = getComputerChoice();
 
     if (playerSelection == computerSelection) {
-        return "Tie Game!"
+        return "Tie Game!";
 
     } else if ((playerSelection == "Rock" && computerSelection == "Scissors")
         || (playerSelection == "Paper" && computerSelection == "Rock")
         || (playerSelection == "Scissors" && computerSelection == "Paper")) {
-        userScore++;
+        userScore++; // attempt to return score? 
         return "You Win! " + playerSelection + " beats " + computerSelection + ".";
 
     } else {
@@ -32,23 +66,25 @@ function playRound() {
 
 }
 
+function displayScore() {
+    currentScore.textContent = "Current score is: User = " + userScore + " Computer = " + computerScore;
+    
+}
 
+function game() {
 
-function game () {
-
-    console.log(playRound());
-    if (userScore < 5 && computerScore < 5){
-        game();
+    if (userScore < 5 && computerScore < 5) {
+        displayScore();
     } else {
         endGame();
     }
 }
 
 
-function endGame(){
+function endGame() {
     if (userScore > computerScore) {
-        console.log("You Win! :)");
+        console.log(currentScore.textContent = "You Win! :) Final Score: User = 5 Computer = " + computerScore);
     } else {
-        console.log("You Lose! :(")
+        console.log(currentScore.textContent = "You Lose! :( Final Score: User = " + userScore + " Computer = 5");
     }
 }
